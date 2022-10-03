5924acabb249d6497eddea8715db0d536fc7ecf4
MATHEUS MURTA DA MOTA <111320301+murtam99@users.noreply.github.com>
2022-10-03 18:26:50 -0300

    curso programador web

---
+++ aula02/prog7.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilo.css"/>
+
+    <title>SENAC - Curso de PHP</title>
+</head>
+<body>
+<div>
+<p>
+   Escreva um programa que leia o preço de um produto. O programa deverá calcular um desconto de 10% na compra a vista. </p>
+
+   <h3>Cadastro de Usuário</h3>
+   <form action="prog7r.php" method="post">
+   Valor: <br>
+   <input type="text" name="valor"><br>
+   Quantidade: <br>
+   <input type="text" name="quantidade"><br>
+   <input type="text" value="Salvar">
+</div>
+</body>
+</html>
---
+++ aula02/prog7r.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilo.css"/>
+    <title>Document</title>
+</head>
+<body>
+<div>
+<p> Escreva um programa que leia o preço de um produto. O programa deverá calcular um desconto de 10% na compra a vista. </p>
+
+<h3>Cadastro de usuário</h3>
+
+<?php
+$login = $_POST["valor"];
+$senha = $_POST["quantidade"] ;
+
+echo "<h3>Informações do usuário</h3>";
+
+echo "Valor: $Valor, Quantidade: $quantidade";
+
+?>
+
+<br>
+<a href="prog7r.php"><h3>Voltar</h3></a>
+
+</div>
+    
+</body>
+</html>
