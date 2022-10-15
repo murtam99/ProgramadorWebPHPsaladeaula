c5372903e543c884865a444b8ac6a6f658b59786
MATHEUS MURTA DA MOTA <111320301+murtam99@users.noreply.github.com>
2022-10-14 21:09:13 -0300

    Exercícios feitos em sala de aula

---
+++ array/array02.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css" />
+    <title>Senac - Curso de PHP</title>
+</head>
+
+<body>
+    <div>
+
+
+
+        <h1 style="text-align: center;">Arrays.</h1>
+        <hr>
+        <br>
+        <pre>
+       <?php 
+
+       //(início, fim, tamanho do passo)
+       //No exemplo esta de 1 em 1
+
+
+       $a = range(1, 10, 1);
+       print_r($a);
+
+     
+       ?>
+       </pre>
+
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
