d0f2b77912c8e1bf1bdda26633418836564f4dd0
MATHEUS MURTA DA MOTA <111320301+murtam99@users.noreply.github.com>
2022-11-18 16:01:15 -0300

    Atualizacoes

--- projeto/estoque1.php
+++ projeto/estoque1.php
@@ -1,60 +1,65 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<?php require "html/head.php" ?>
+
+	<?php 
+		include "html/head.php";
+		
+	?>
+ 
 </head>
-	<!--  -->
+	
 	<body>
-		<?php 
-		include "html/header.php"; 
-		require_once "src/conexao.php";
+		<?php include "html/header.php";
+			require_once "src/conexao.php";
 
-		$id = isset($_GET['id']) ? $_GET['id'] : 0;
+			$id= isset($_GET['id'])? $_GET['id'] : 0;
 
-		$sql_code = "SELECT * FROM produtos LEFT JOIN estoque ON idproduto = id_produto WHERE idproduto = '$id'";
-		$sql_query = $conexao->query($sql_code);
-				
+			$sql_code = "SELECT * FROM produtos LEFT JOIN estoque ON idproduto = id_produto WHERE idproduto = '$id'";
+
+			$sql_query = $conexao->query($sql_code);
 		?>
 		<main>
 			<h1>Estoque</h1>
-			<h3>Lista de Produtos Cadastrados</h3>
-			<table class = "table table-bordered">
-				<tr>
-					<th scope="col"> ID </th>
-					<th scope="col"> FOTO </th>
-					<th scope="col"> NOME </th>
-					<th scope="col"> TIPO </th>
-					<th scope="col"> CATEGORIA </th>
-					<th scope="col"> FABRICANTE </th>
-					<th scope="col"> QUANTIDADE </th>
-					<th scope="col"> REGISTRO </th>
-					<th scope="col"> DATA </th>
-					<th scope="col"> VALOR COMPRA </th>
-					<th scope="col"> VALOR VENDA </th>
-					
-				</tr>
-				<?php 
-					while($produto = $sql_query->fetch_assoc()){
-
-				?>
-				<tr>
-						<td><?=$produto['idproduto']?></td>
-						<td><img height = "50" src="<?=$produto['foto']?>"></td>
-						<td><?=$produto['nome']?></td>
-						<td><?=$produto['tipo']?></td>
-						<td><?=$produto['categoria']?></td>
-						<td><?=$produto['fabricante']?></td>
-						<td><?=$produto['qtd']?></td>
-						<td><?=$produto['registro']?></td>
-						<td><?=$produto['data_registro']?></td>
-						<td><?=$produto['valor_compra']?></td>
-						<td><?=$produto['valor_venda']?></td>
-				</tr>
-					<?php }?>
-			</table>
+			<h3>Lista cadastrados</h3>
+			<div class="table-responsive">
+				<table class="table table-striped">
+					<tr>
+						<th>ID</th>
+						<th>FOTO</th>
+						<th>NOME</th>
+						<th>TIPO</th>
+						<th>CATEGORIA</th>
+						<th>FABRICANTE</th>
+						<th>QTD</th>
+						<th>REGISTRO</th>
+						<th>DATA</th>
+						<th>VALOR COMPRA</th>
+						<th>VALOR VENDA</th>
+					</tr>
+					<?php
+						while ($produto = $sql_query->fetch_assoc()){
+					?>
+					<tr>
+						<td><?= $produto['idproduto']?></td>
+						<td><img height= '50' src = " <?= $produto['foto']?>"></td>
+						<td><?= $produto['nome']?></td>
+						<td><?= $produto['tipo']?></td>
+						<td><?= $produto['categoria']?></td>
+						<td><?= $produto['fabricante']?></td>
+						<td><?= $produto['qtd']?></td>
+						<td><?= $produto['registro']?></td>
+						<td><?= $produto['data_registro']?></td>
+						<td><?= $produto['valor_compra']?></td>
+						<td><?= $produto['valor_venda']?></td>
+					</tr>
+						<?php }?>
+				</table>
+			</div>
 			<div class="container-fluid">
             <h3>Registro de Produto</h3>
-            <form class="row g-3 container-fluid" action="src/controller/estoque_bd/editarEstoque.php" method="post">
+            <form class="row g-3 container-fluid" action="" method="post">
 
                 <input type="text" class="form-control" id="id_produto" name="id_produto" value="<?= $produto['idproduto'] ?>" hidden>
 
@@ -89,9 +94,11 @@
             </form>
         </div>
 		</main>
-		<?php include "html/rodape.php" ?>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+		
+		<?php include "html/footer.php" ?>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
