97633663387d97ca93ee6af52f01869ee5156e20
MATHEUS MURTA DA MOTA <111320301+murtam99@users.noreply.github.com>
2022-11-21 21:32:43 -0300

    atualizacoes

--- projeto/maisDetalhes1.php
+++ projeto/maisDetalhes1.php
@@ -29,11 +29,11 @@
 			
 			use projeto;
 
-RENAME TABLE produto TO produtos;
-ALTER TABLE produtos ADD categoria varchar(30) AFTER tipo;
+RENAME TABLE produto to produtos;
+ALTER TABLE produtos add categoria varchar(30) AFTER tipo;
 ALTER TABLE produtos ADD fabricante varchar(30) AFTER categoria;
 ALTER TABLE estoque CHANGE valor valor_compra double;
-ALTER TABLE estoque ADD valor_venda double AFTER valor_compra;
+ALTER TABLE estoque ADD valor_venda double AFTER valor
  -->
 	
 	<body>
